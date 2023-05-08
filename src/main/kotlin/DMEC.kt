fc7ee35bdb01d5ffda1bcc151742906aae0d94e9
BuildTools <unconfigured@null.spigotmc.org>
2023-05-08 11:35:17 +0100

    Commented

--- src/main/kotlin/DMEC.kt
+++ src/main/kotlin/DMEC.kt
@@ -4,6 +4,7 @@ object Doormechanism { // Controla o estado do mecanismo de abertura da porta.
 
         // Inicia a classe, estabelecendo os valores iniciais.
         fun init() {
+            OC = 0
             var HAL = HAL
             var SEM = SerialEmitter
         }
@@ -14,18 +15,18 @@ object Doormechanism { // Controla o estado do mecanismo de abertura da porta.
         fun open(velocity: Int) {
             OC = 1
             var cmd = velocity.shl(1)
-            SerialEmitter.send(SerialEmitter.Destination.DOOR,cmd or OC)
+            SerialEmitter.send(SerialEmitter.Destination.DOOR,cmd or OC)  // 1velocity
 
         }
 
         // Envia comando para fechar a porta, com o parâmetro de velocidade
         fun close(velocity: Int) {
             var cmd = velocity.shl(1)
-            SerialEmitter.send(SerialEmitter.Destination.DOOR,cmd)
+            SerialEmitter.send(SerialEmitter.Destination.DOOR,cmd or OC) //0velocity
         }
 
         // Verifica se o comando anterior está concluído
         fun finished() : Boolean {
-            return !SerialEmitter.isBusy()
+            return !SerialEmitter.isBusy()  //if not busy, then is finished
         }
     }
